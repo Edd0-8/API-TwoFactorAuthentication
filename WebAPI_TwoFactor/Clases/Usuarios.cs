9cca81f7e3afb87cc4255d0ca1e89a9c52a0ba80
eespinoz4 <eespinoza@netcore.cl>
2024-06-06 13:30:56 -0400

    Subida Proyecto Completo

---
+++ WebAPI_TwoFactor/Clases/Usuarios.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebAPI_TwoFactor.Clases
+{
+    public class Usuarios
+    {
+
+        //Cadena de conexion a la base de datos 
+        public Usuarios()
+        {
+            //Aqui va la cadena de conexion del servidor SQL Server
+            //Ejemplo: Data Source=NombredelServidorLocal;Initial Catalog=NombreDeLaDB;Integrated Security=True
+            AccesoDatos.cadenaConexion = @"";
+        }
+
+        #region SetSecret
+        //Metodo para actualizar el campo 2FA de la tabla Usuarios donde el email sea igual a el correo 
+        public void SetSecret(string email, string code, string aplicacion)
+        {
+
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@email", email),
+                new SqlParameter("@etiqueta", aplicacion),
+                new SqlParameter("code", code) 
+            };
+
+            AccesoDatos.ExecuteStoredProcedure("SP_CREATE_CODE", parameters);
+        }
+        #endregion
+        #region GetSecret
+        //Metodo para obtener el token de 2FA
+        public string GetSecret(string email,string aplicacion)
+        {
+            DataTable dt = AccesoDatos.GetTmpDataTable($"SELECT Token FROM dbo.TwoFactorSecret WHERE Etiqueta = '{aplicacion}' and IdUsuario = (SELECT Id FROM Usuario WHERE Email = '{email}')");
+            if (dt.Rows.Count == 1)
+            {
+                return dt.Rows[0]["Token"].ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+        #endregion
+    }
+}
---
+++ WebAPI_TwoFactor/Controllers/TwoFactorController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TwoFactorAuthNet;
+using TwoFactorAuthNet.Providers.Qr;
+using WebAPI_TwoFactor.Clases;
+
+namespace WebAPI_TwoFactor.Controllers
+{
+    public class TwoFactorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region QR HTML
+        //Genera un codigo QR y a traves de una etiqueta HTML
+        [HttpGet, Route("GetQRCode")]
+        public string GetQRCode(string email, string Aplicacion)
+        {
+            var tfa = new TwoFactorAuth(Aplicacion, 6, 30, Algorithm.SHA1, new ImageChartsQrCodeProvider());
+            var secret = tfa.CreateSecret(160);
+
+            Usuarios usu = new Usuarios();
+            usu.SetSecret(email, secret, Aplicacion);
+            usu = null;
+
+            string imgQR = tfa.GetQrCodeImageAsDataUri(email, secret);
+            string imgHTML = $"<img src='{imgQR}'>";
+            return imgHTML;
+        }
+        #endregion
+        #region QR Swagger
+        //Genera un codigo QR y lo muestra en Swagger (prueba)
+        [HttpGet, Route("GetQRCodeAsImage")]
+        public FileContentResult GetQRCodeAsImage(string email, string aplicacion)
+        {
+            var tfa = new TwoFactorAuth(aplicacion, 6, 30, Algorithm.SHA1, new ImageChartsQrCodeProvider());
+            var secret = tfa.CreateSecret(160);
+
+            Usuarios usu = new Usuarios();
+            usu.SetSecret(email, secret, aplicacion);
+            usu = null;
+            string imgQR = tfa.GetQrCodeImageAsDataUri(email, secret);
+            imgQR = imgQR.Replace("data:image/png;base64,", "");
+            byte[] picture = Convert.FromBase64String(imgQR);
+            return File(picture, "image/png");
+        }
+        #endregion
+        #region Validar QR
+        //Valida el codigo de autenticacion con el usuario
+        [HttpGet, Route("ValidarQRCode")]
+        public bool ValidarCodigo(string email, string aplicacion, string code)
+        {
+            Usuarios usu = new Usuarios();
+            string secret = usu.GetSecret(email, aplicacion);
+            usu = null;
+
+            var tfa = new TwoFactorAuth("MiCodigo {IdAplicacion}", 6, 30, Algorithm.SHA1);
+            return tfa.VerifyCode(secret, code, 2);
+        }
+        #endregion
+
+    }
+}
